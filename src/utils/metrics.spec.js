540b84c7d341cdbce56d72ae9251bfc6cc2d9f70
Ajain Vivek <ajainvivek07@gmail.com>
2019-09-01 17:49:01 +1000

    tests: fix metrics

--- src/utils/metrics.spec.js
+++ src/utils/metrics.spec.js
@@ -18,14 +18,14 @@ describe('Metrics', () => {
   it('should start observing and measuring metrics for fn when enabled', () => {
     expect.assertions(2);
     const mockFunc = jest.fn();
-    metrics.measure(mockFunc, true);
+    metrics.measure(mockFunc, 'true');
     expect(perf.performance.timerify).toHaveBeenCalled();
     expect(mockFunc).toHaveBeenCalled();
   });
 
   it('should not start observing and measuring metrics for fn when disabled', () => {
     expect.assertions(2);
-    config.enableMetrics = false;
+    config.enableMetrics = 'false';
     const mockFunc = jest.fn();
     metrics.measure(mockFunc, false);
     expect(perf.performance.timerify).not.toHaveBeenCalled();
